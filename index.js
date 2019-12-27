f5f4fbde2e956764ed97aa1b75ca6ea32a86df1b
Aylin Demir <aylin.de@outlook.com>
2019-12-27 21:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function removeFromCart(name) {
 for(let i =0;i<cart.length;i++){
     if(getCart()[i].itemName === name){
         cart.splice(i,1)
-        return cart
+        return getCart()
       }else{
         return "That item is not in your cart."
       }
